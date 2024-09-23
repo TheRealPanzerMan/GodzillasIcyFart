f9fdea5c509caa42cc4fae4dc639a877c313c6a0
TheRealPanzerMan <theiiitopiiigun@gmail.com>
2024-09-23 23:21:29 +0500

    v0.0.2
    релиз

--- calling.js
+++ calling.js
@@ -1,5 +1,30 @@
-//  Вы можете добавить здесь  дополнительные   действия   после   завершения   "звонка"
-setTimeout(function() {
-    //  например, переход на другую страницу
-    window.location.href = 'index.html'; 
-  }, 5000); //  5 секунд
+
+ //  Дополнительные   действия   после   завершения   "звонка"
+setTimeout(callGodzilla, 1000); //  1 секунда
+
+function callGodzilla() {
+    // Список доступных звуков
+    const soundFiles = [
+      'sound/bigfart.mp3',
+      'sound/roar.mp3',
+      'sound/roar2.mp3',
+      'sound/calling.mp3',
+      'sound/calling2.ogg',
+      'sound/baufukushima.mp3',
+      'sound/baufukushima2.mp3',
+      'sound/baufukushima3.mp3',
+      'sound/song1.mp3',
+      'sound/song2.mp3'
+    ];
+  
+    // Выбор случайного звука из списка
+    const randomSoundIndex = Math.floor(Math.random() * soundFiles.length);
+    const randomSound = soundFiles[randomSoundIndex];
+  
+    // Воспроизведение случайного звука
+    const audio = new Audio(randomSound);
+    audio.play();
+  
+    // Переход на следующую страницу (calling.jpg)
+    //window.location.href = 'calling.html';
+  }
--- script.js
+++ script.js
@@ -3,12 +3,35 @@ function playFart() {
     const audio = new Audio('fart.mp3');
     audio.play();
   }
+
+//  Список доступных звуков
+const soundFiles = [
+    'sound/bigfart.mp3',
+    'sound/roar.mp3',
+    'sound/roar2.mp3',
+    'sound/calling.mp3',
+    'sound/calling2.ogg',
+    'sound/baufukushima.mp3',
+    'sound/baufukushima2.mp3',
+    'sound/baufukushima3.mp3',
+    'sound/song1.mp3',
+    'sound/song2.mp3'
+  ];
   
+  //  Функция для воспроизведения случайного звука и перехода на следующую страницу
   function callGodzilla() {
-    // Замените 'call.mp3' на ваше имя файла
-    const audio = new Audio('call.mp3');
+    // Выбор случайного звука из списка
+    const randomSoundIndex = Math.floor(Math.random() * soundFiles.length);
+    const randomSound = soundFiles[randomSoundIndex];
+  
+    // Воспроизведение случайного звука
+    const audio = new Audio(randomSound);
     audio.play();
   
-    // Переход на следующую страницу (calling.jpg)
-    window.location.href = 'calling.html';
+    //  Показать   "calling-screen"   и   скрыть   "godzilla-screen"
+    document.getElementById('godzilla-screen').style.display = 'none';
+    document.getElementById('calling-screen').style.display = 'flex';
+  
+    //  Показать   текст   "GODZILLA'S   ICY   FART"   и   т.д.
+    document.getElementById('calling-info').style.display = 'block';
   }
